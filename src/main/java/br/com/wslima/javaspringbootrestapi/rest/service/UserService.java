d0d210689c457edd71bcb8e06c4bf4fa1b64f5ca
Kennedy Lima <kennedy.wslima@gmail.com>
2025-06-16 00:43:37 -0400

    feature[user] criando service e impl de user

--- src/main/java/br/com/wslima/javaspringbootrestapi/rest/service/UserService.java
+++ src/main/java/br/com/wslima/javaspringbootrestapi/rest/service/UserService.java
@@ -1,4 +1,16 @@
 package br.com.wslima.javaspringbootrestapi.rest.service;
 
-public class UserService {
+import br.com.wslima.javaspringbootrestapi.persistence.model.User;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserService {
+    User save(User user);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUuid(UUID uuid);
+    List<User> findAll();
+    void deleteById(UUID uuid);
+
 }
--- src/main/java/br/com/wslima/javaspringbootrestapi/rest/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/wslima/javaspringbootrestapi/rest/service/impl/UserServiceImpl.java
@@ -1,4 +1,47 @@
 package br.com.wslima.javaspringbootrestapi.rest.service.impl;
 
-public class UserServiceImpl {
+import br.com.wslima.javaspringbootrestapi.persistence.model.User;
+import br.com.wslima.javaspringbootrestapi.persistence.repository.UserRepository;
+import br.com.wslima.javaspringbootrestapi.rest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public Optional<User> findByUuid( UUID uuid) {
+        return userRepository.findByUuid(uuid);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(UUID uuid) {
+        userRepository.deleteById(uuid);
+    }
 }
