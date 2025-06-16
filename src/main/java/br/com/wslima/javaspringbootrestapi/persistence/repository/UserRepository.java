384c24a6783c3512391cf9b7f96034e3643a8e36
Kennedy Lima <kennedy.wslima@gmail.com>
2025-06-16 00:41:20 -0400

    feature[user] criando o repository de user

--- src/main/java/br/com/wslima/javaspringbootrestapi/persistence/repository/UserRepository.java
+++ src/main/java/br/com/wslima/javaspringbootrestapi/persistence/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package br.com.wslima.javaspringbootrestapi.persistence.repository;
 
-public class UserRepository {
+import br.com.wslima.javaspringbootrestapi.persistence.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findByUuid(UUID uuid);
 }
